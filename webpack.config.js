83ef62df7787d2d70eaa567cbc14ed88d4a20ff0
Brandon Furtwangler <brandon.furtwangler@hotmail.com>
2016-10-11 15:44:55 -0700

    setup webpack and npm package

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: './bin/',
+    publicPath: '/bin/',
+    filename: 'bundle.js',
+    sourceMapFilename: '[file].map',
+    devtoolModuleFilenameTemplate: 'webpack:///[resource-path]?[loaders]',
+  },
+  devtool: 'source-map',
+};
